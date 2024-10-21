bc6f29f0d02e040830efc7fc910aa1364793d425
Siyasanga <dsinethemba@gmail.com>
2024-10-21 21:34:11 +0200

    refactore the server and got it to work.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors';
 
 (async () => {
-  const fastify = Fastify({});
+  const server = Fastify({ logger: true });
+  server.register(cors);
 
-  fastify.get("/hello", async () => {
+
+  server.get("/hello", async () => {
     return { hello: "world" };
   });
 
   try {
-    await fastify.listen({ port: 3000 });
+    await server.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server is running on http://localhost:3000');
   } catch (err) {
-    fastify.log.error(err);
+    server.log.error(err);
+    process.exit(1);
   }
 })();
